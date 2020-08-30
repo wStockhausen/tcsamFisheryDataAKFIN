ae1fa49c779b5e585d6389259487048098b0e4a0
William Stockhausen <williamstockhausen@MacBook-Pro.local>
2020-08-29 23:54:53 -0700

    Corrected errors in akfin.ScaleInputSSs.R. Incremented version to 2020.08.29.

--- R/akfin.ScaleInputSSs.R
+++ R/akfin.ScaleInputSSs.R
@@ -8,16 +8,15 @@
 #' @param .ss_max - maximum input sample size allowed
 #'
 #' @return dataframe or tibble with the same column order as the input, but with
-#' additional columns \code{ss_tot} and \code{ss_scl}. Column \code{ss} contains the scaled
+#' additional columns \code{ss_orig}, \code{ss_tot}, and \code{ss_scl}. Column \code{ss} contains the scaled
 #' sample sizes, column \code{ss_scl} contains the scale used to convert the original
-#' sample sizes.
+#' sample sizes, and column \code{ss_orig} contains the original sample sizes.
 #'
 #' @details The scale factor \code{ss_scl} and max input sample size \code{ss_max} are
 #' used to determine input annual sample sizes for size composition data using the formula \cr
-#'              \code{inp_ss = min(max_ss,max_ss*(ss_tot/scs_scl))*ss/ss_tot} \cr
+#'              \code{inp_ss = min(max_ss,max_ss\*(ss_tot/ss_scl)\*(ss/ss_tot)} \cr
 #' where \code{ss} is the original sample size (the total number of crab measured) by factor
-#' combination and \code{ss_tot} is the total sample size by year and sex.
-#'
+#' combination and \code{ss_tot} is the total sample size by year.
 #' @note \code{dfrSS} should have (at least) the columns
 #' \itemize{
 #'   \item{year}
@@ -42,12 +41,12 @@ akfin.ScaleInputSSs<-function(dfrSS,
            dplyr::summarize(ss_tot=wtsUtilities::Sum(ss)) %>%
            dplyr::mutate(ss_scl=.ss_max*(ss_tot/.ss_scl)) %>%
            dplyr::group_by(year) %>%
-           dplyr::mutate(ss_scl=min(ss_max,ss_scl)) %>%
+           dplyr::mutate(ss_scl=min(.ss_max,ss_scl)) %>%
            dplyr::ungroup();
   #--apportion scaled sample sizes by year, sex and other factors
   dfrSSs_inp<-dfrSS %>%
                  dplyr::inner_join(dfrSSp,by=c("year")) %>%
-                 dplyr::mutate(ss=ss_scl*(ss/ss_tot)) %>%
-                 dplyr::relocate(tidyselect::any_of(c(names(dfrSS),"ss_tot","ss_scl")));
+                 dplyr::mutate(ss_orig=ss,ss=ss_scl*(ss/ss_tot)) %>%
+                 dplyr::relocate(tidyselect::any_of(c(names(dfrSS),"ss_orig","ss_tot","ss_scl")));
   return(dfrSSs_inp);
 }
